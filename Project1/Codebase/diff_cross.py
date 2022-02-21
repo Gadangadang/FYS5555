3c7782ce6fb821e75fc1722c45661e36da4e4a00
Sakarias Frette <sakarias.frette@outlook.com>
2022-02-21 13:43:42 +0100

    fixed puny bugs

--- Project1/Codebase/diff_cross.py
+++ Project1/Codebase/diff_cross.py
@@ -16,46 +16,48 @@ class DiffCrossection:
         self.e = 0.31345
         self.costhetaW = np.cos(self.thetaw)
 
-        self.gz = self.e / (self.sinthetaW * np.cos(self.thetaw))
         self.g = self.e / self.sinthetaW
+        self.gz = self.gz / np.cos(self.thetaw)
 
         # Z boson vertex constants
         self.g_Amu = self.gz * 0.5 * (-0.5)
-        self.g_Vmu = self.gz * 0.5 * (-0.5 - 2 * (-1) * self.sinthetaW ** 2)
+        self.g_Vmu = self.gz * 0.5 * (-0.5 - 2 * (-1) * self.sinthetaW**2)
         self.g_Ab = self.gz * 0.5 * (-0.5)
-        self.g_Vb = self.gz * 0.5 * (-0.5 - 2 * (-1 / 3) * self.sinthetaW ** 2)
+        self.g_Vb = self.gz * 0.5 * (-0.5 - 2 * (-1 / 3) * self.sinthetaW**2)
 
         self.four_gs = self.g_Amu * self.g_Vmu * self.g_Ab * self.g_Vb
-        
-        self.A = self.g_Amu ** 2 + self.g_Vmu ** 2
-        self.Atilde = self.g_Amu ** 2 - self.g_Vmu ** 2
-        self.B = self.g_Ab ** 2 + self.g_Vb ** 2
-        self.Btilde = self.g_Ab ** 2 - self.g_Vb ** 2
+
+        self.A = self.g_Amu**2 + self.g_Vmu**2
+        self.Atilde = self.g_Amu**2 - self.g_Vmu**2
+        self.B = self.g_Ab**2 + self.g_Vb**2
+        self.Btilde = self.g_Ab**2 - self.g_Vb**2
 
         # Energy and com split
         self.Ecm = energy_centermass  # GeV
         self.E2 = (self.Ecm / 2) ** 2
         self.E = self.Ecm / 2
-        self.s = self.Ecm ** 2  # GeV
+        self.s = self.Ecm**2  # GeV
 
         # Amplitude constants
-        self.m1const = self.e ** 4 / (9 * self.s ** 2)
-        self.m2const = self.g ** 2 / (
-            self.costhetaW ** 2 * (self.s - self.mZ ** 2) ** 2
-        )
+        self.m1const = self.e**4 / (9 * self.s**2)
+        self.m2const = self.gz**4 / (self.s - self.mZ**2) ** 2
         self.m1m2const = (
-            self.e ** 2
-            * self.g ** 2
-            / (3 * self.costhetaW ** 2 * self.s * (self.s - self.mZ ** 2))
+            self.e**2
+            * self.g**2
+            / (3 * self.costhetaW**2 * self.s * (self.s - self.mZ**2))
         )
 
         # Momentum dot products and def of momentum
-        self.p = np.sqrt(self.E2 - self.m_mu ** 2)
-        self.p_ = np.sqrt(self.E2 - self.m_b ** 2)
-        self.pp_kk_ = (self.E ** 2 - self.p * self.p_ * self.costheta) ** 2
-        self.pk_p_k = (self.E ** 2 + self.p * self.p_ * self.costheta) ** 2
-        self.pk = self.E2 + self.p ** 2
-        self.p_k_ = self.E2 + self.p_ ** 2
+        self.p = np.sqrt(self.E2 - self.m_mu**2)
+        self.p_ = np.sqrt(self.E2 - self.m_b**2)
+        self.pp_kk_ = (self.E**2 - self.p * self.p_ * self.costheta) ** 2
+        self.pk_p_k = (self.E**2 + self.p * self.p_ * self.costheta) ** 2
+        self.pk = self.E2 + self.p**2
+        self.p_k_ = self.E2 + self.p_**2
+
+        comp_hep_data = np.loadtxt("comphep_200GeV.txt", skiprows=3)
+        self.comphep_degree = comp_hep_data[:, 0]
+        self.comphep_diff = comp_hep_data[:, 1]
 
     def M1squared(self):
         return (
@@ -64,9 +66,9 @@ class DiffCrossection:
             * (
                 self.pp_kk_
                 + self.pk_p_k
-                + self.m_b ** 2 * self.pk
-                + self.m_mu ** 2 * self.p_k_
-                + 2 * self.m_b * self.m_mu
+                + self.m_b**2 * self.pk
+                + self.m_mu**2 * self.p_k_
+                + 2 * self.m_b**2 * self.m_mu**2
             )
         )
 
@@ -76,9 +78,9 @@ class DiffCrossection:
             * 8
             * (
                 self.A * self.B * (self.pp_kk_ + self.pk_p_k)
-                + self.B * self.m_b ** 2 * self.pk
-                + self.A * self.m_mu ** 2 * self.p_k_
-                + 2 * self.m_b ** 2 * self.m_mu ** 2 * self.A * self.B
+                - self.Atilde * self.B * self.m_b**2 * self.pk
+                - self.A * self.Btilde * self.m_mu**2 * self.p_k_
+                + 2 * self.m_b**2 * self.m_mu**2 * self.Atilde * self.Btilde
                 - 4 * self.four_gs * (self.pp_kk_ - self.pk_p_k)
             )
         )
@@ -90,9 +92,9 @@ class DiffCrossection:
             * (
                 self.g_Ab * self.g_Vb * self.pk_p_k
                 + self.g_Amu * self.g_Vmu * self.pp_kk_
-                + self.g_Vb * self.m_b ** 2 * self.pk
-                + self.g_Vmu * self.m_mu ** 2 * self.p_k_
-                + 2 * self.m_b ** 2 * self.m_mu ** 2 * self.g_Vb * self.g_Vmu
+                + self.g_Vb * self.m_b**2 * self.pk
+                + self.g_Vmu * self.m_mu**2 * self.p_k_
+                + 2 * self.m_b**2 * self.m_mu**2 * self.g_Vb * self.g_Vmu
                 - self.g_Amu * self.g_Ab * (self.pp_kk_ - self.pk_p_k)
             )
         )
@@ -109,7 +111,8 @@ class DiffCrossection:
         )
 
     def plot_cross(self):
-        plt.plot(self.costheta, self.diff_cross(), label=r"d$\sigma$/cos($\theta$) ")
+        plt.plot(self.costheta, self.diff_cross(), label="Analytical")
+        plt.plot(self.comphep_degree, self.comphep_diff, "r--", label="CompHEP")
         plt.xlabel(r"cos($\theta$) ")
         plt.ylabel(r"$\frac{d\sigma}{d \cos{(\theta)}}$")
         plt.legend()
