1f0b1099d88fefe74e6a147acb45da2faae092c2
Sakarias Frette <sakarias.frette@outlook.com>
2022-04-29 16:58:58 +0200

    added more image info in plot

--- Project3/Codebase/example_plots.py
+++ Project3/Codebase/example_plots.py
@@ -25,22 +25,27 @@ plt.vlines(x = 1.5, ymin = 3, ymax=5)
 plt.scatter(Outlier[:,0],Outlier[:,1], color="y", label="Outliers")
 plt.scatter(Inlier[:,0],Inlier[:,1], color="k", label="Inliers")
 plt.ylim(0, 5)
-
+plt.xlabel("x")
+plt.ylabel("y")
 plt.legend()
+plt.title("Classification example with correct target model")
 
 plt.savefig("../figures/correct_class.pdf")
 
 plt.show()
 
 
-plt.hlines(y = 3, xmin = 1.5, xmax=3)
+plt.hlines(y = 3, xmin = 3, xmax=4)
 
-plt.vlines(x = 1.5, ymin = 3, ymax=5)
+plt.vlines(x = 3, ymin = 3, ymax=5)
 plt.scatter(Outlier[:,0],Outlier[:,1], color="y", label="Outliers")
 plt.scatter(Inlier[:,0],Inlier[:,1], color="k", label="Inliers")
 plt.ylim(0, 5)
-
+plt.xlabel("x")
+plt.ylabel("y")
 plt.legend()
+
+plt.title("Classification example with wrong target model")
 
 plt.savefig("../figures/wrong_class.pdf")
 
