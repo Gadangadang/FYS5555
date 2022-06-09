6575ddb5253c869d57ff1b5f19b14450e2b7003c
Sakarias Frette <sakarias.frette@outlook.com>
2022-06-09 11:12:54 +0200

    removed old code, and moved unused to separate folder

--- Project3/Codebase/ex.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-
-model = tf.keras.models.load_model("ex_model.h5")
-
-tf.keras.utils.plot_model(model, to_file="../figures/results/ae_ex_model_plot.png",
-                          show_shapes=True, show_layer_names=True, expand_nested=True)
